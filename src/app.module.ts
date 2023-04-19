7fe8e24759b3b91119b8f58a04ba9e1f77f49611
Cem <kanmaz.cem@gmail.com>
2023-04-19 19:25:38 +0200

    Authentication, database docker, prisma,

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { NestFactory } from '@nestjs/core';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Injectable, ParseIntPipe, Post, Req } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService : AuthService) {
+
+        
+    }
+
+    @Post('signup')
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
+    }
+
+    @Post('signin')
+    signin(@Body() dto: AuthDto) {
+        console.log(dto);
+        return this.authService.signin(dto);
+    }
+}
