d8e76bb5031a21b82d6846e2345ccb8d081f1b42
Mian Khubaib <miankhubaib63@gmail.com>
2023-06-06 15:15:02 +0500

    Prisma: Updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Patch,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
@@ -17,14 +11,11 @@ export class UserController {
   constructor(private userService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
-    return {"user" : user};
+    return { user: user };
   }
 
   @Patch()
-  editUser(
-    @GetUser('id') userId: number,
-    @Body() dto: EditUserDto,
-  ) {
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
     return this.userService.editUser(userId, dto);
   }
 }
