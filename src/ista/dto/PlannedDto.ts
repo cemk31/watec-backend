93600776dca0fc1cb6f69b8651be9cf300676f85
cemk31 <kanmaz.cem@googlemail.com>
2023-09-21 15:36:06 +0200

    make some changes

--- src/ista/dto/PlannedDto.ts
+++ src/ista/dto/PlannedDto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsInt, IsString, IsDate, IsOptional, IsNumber } from "class-validator";
 import { CustomerContactDto } from "./CustomerContactDto";
 import { OrderDto } from "./ista.order.dto";
+import { Transform, TransformFnParams } from 'class-transformer';
 
 export class PlannedDto {
     @ApiProperty({ example: 1 })
@@ -22,6 +23,13 @@ export class PlannedDto {
     @ApiProperty({ example: '2023-01-01T00:00:00Z' })
     @IsOptional()
     @IsDate()
+    @Transform(({ value }: TransformFnParams) => {
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        throw new Error(`Invalid date value: ${value}`);
+      }
+      return date;
+    })
     setOn: Date;
   
     @ApiProperty({ type: () => [CustomerContactDto] })
@@ -31,16 +39,38 @@ export class PlannedDto {
     @ApiProperty({ example: '2023-01-01T00:00:00Z' })
     @IsOptional()
     @IsDate()
+    @Transform(({ value }: TransformFnParams) => {
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        throw new Error(`Invalid date value: ${value}`);
+      }
+      return date;
+    })
     detailedScheduleDate: Date;
   
     @ApiProperty({ example: '2023-01-01T00:00:00Z' })
     @IsOptional()
     @IsDate()
+    @Transform(({ value }: TransformFnParams) => {
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        throw new Error(`Invalid date value: ${value}`);
+      }
+      return date;
+    })
     detailedScheduleTimeFrom?: Date;
   
+    
     @ApiProperty({ example: '2023-01-01T00:00:00Z' })
     @IsOptional()
     @IsDate()
+    @Transform(({ value }: TransformFnParams) => {
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        throw new Error(`Invalid date value: ${value}`);
+      }
+      return date;
+    })
     detailedScheduleTimeTo?: Date;
   
     @ApiProperty({ example: 'Delay Reason' })
